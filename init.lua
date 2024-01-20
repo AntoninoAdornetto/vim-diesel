f7c989b607c9a12fa5e1e187cf45191d2532bccc
AntoninoAdornetto <armaninollc@protonmail.com>
2024-01-19 23:21:24 -0500

    fix: vim test configuration

--- init.lua
+++ init.lua
@@ -254,6 +254,11 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  {
+    'vim-test/vim-test',
+    event = "BufRead",
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -342,6 +347,13 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Test execution (vim-test) keymaps
+vim.api.nvim_set_keymap('n', '<leader>t', ':TestNearest<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>T', ':TestFile<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>a', ':TestSuite<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', ':TestLast<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>g', ':TestVisit<CR>', { silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
