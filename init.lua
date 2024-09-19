98d939dcaffd92e34a56e8466465d1ada5cc90b0
AntoninoAdornetto <armaninollc@protonmail.com>
2024-09-18 18:53:28 -0600

    refactor: rm/add/update plugins & settings

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- [[ Options, Keymaps, Auto Commands ]]
+require 'core'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'plugins'
---
+++ lua/theme/tokyo_night.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      require('tokyonight').setup {
+        style = 'night',
+        transparent = true,
+        styles = {
+          keywords = { italic = true },
+          functions = { bold = true },
+          sidebars = 'transparent',
+          floats = 'transparent',
+        },
+
+        on_colors = function(_colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights tokyonight.Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+
+        cache = true, -- When set to true, the theme will be cached for better performance
+
+        ---@type table<string, boolean|{enabled:boolean}>
+        plugins = {
+          -- enable all plugins when not using lazy.nvim
+          -- set to false to manually enable/disable plugins
+          all = package.loaded.lazy == nil,
+          -- uses your plugin manager to automatically enable needed plugins
+          -- currently only lazy.nvim is supported
+          auto = true,
+          -- add any plugins here that you want to enable
+          -- for all possible plugins, see:
+          --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+          -- telescope = true,
+        },
+      }
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- See `:help lualine.txt`
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'tokyonight-night',
+        },
+      }
+    end,
+  },
+}
