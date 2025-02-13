1ccad1425287d2a67770cd708696509e9d4b0d9d
AntoninoAdornetto <armaninollc@protonmail.com>
2025-02-13 14:31:41 -0700

    chore: use specific formatters for go file types

--- lua/plugins/auto_format.lua
+++ lua/plugins/auto_format.lua
@@ -32,7 +32,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      sql = { 'sqlfmt' },
+      go = { 'gofumpt', 'goimports_reviser', 'golines' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
