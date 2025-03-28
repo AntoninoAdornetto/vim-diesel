83969da32b2944b2bdb6c4a17954a9bb5dd83ca9
AntoninoAdornetto <armaninollc@protonmail.com>
2025-03-28 12:15:00 -0600

    fix: virtual diagnostic text was not appearing in buffer

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,6 +7,12 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.diagnostic.config {
+  virtual_text = true, -- Show inline errors/warnings
+  signs = true, -- Enable diagnostic signs in the sign column
+  underline = true, -- Underline problematic code
+  update_in_insert = false, -- Don't update diagnostics while typing
+}
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
